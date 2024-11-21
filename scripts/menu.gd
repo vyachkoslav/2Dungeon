2696e755a3495094659d8e85c746c1e61f058c8a
omena1967 <joonasv.kontio@gmail.com>
2024-11-21 14:24:04 +0200

    Main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
