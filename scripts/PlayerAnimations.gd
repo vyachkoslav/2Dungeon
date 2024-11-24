58febd85239d169be0ae9077cd1fa5729c01b100
wimpula0 <vilma.vayrynen@edu.turkuamk.fi>
2024-11-24 04:20:08 +0200

    Added player HP and HUD to levels
    Players hurt animation doesn't yet function when func decrease_health() is called. Pause button also leads nowhere.

--- scripts/PlayerAnimations.gd
+++ scripts/PlayerAnimations.gd
@@ -1,4 +1,7 @@
 extends CharacterBody2D
+@export var hearts: Array[Node]
+
+#Animations
 
 func jump_animation() -> void:
 	$AnimatedSprite2D.play("jump")
@@ -9,8 +12,7 @@ func fall_animation() -> void:
 	$AnimatedSprite2D.play("fall")
 	$AnimatedSprite2D.play("fall_loop")
 	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta: float) -> void:
 	move_and_slide()
 	
@@ -27,3 +29,19 @@ func _process(_delta: float) -> void:
 		jump_animation()
 	elif velocity.y > 0 and !is_on_floor():
 		fall_animation()
+
+
+#Health System
+
+var lives = 3
+
+func decrease_health():
+	lives -= 1
+	for h in 3:
+		if (h < lives):
+			hearts[h].show()
+		else:
+			hearts[h].hide()
+	$AnimatedSprite2D.play("hurt")
+	if lives == 0:
+		get_tree().reload_current_scene()
