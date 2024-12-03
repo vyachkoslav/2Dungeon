9ccd63fa72a7884657e5a9b58deec0731059de5c
ElDiamondo <utile.peach2@gmail.com>
2024-12-03 15:38:39 +0200

    Added the edited SFX to game.
    Player makes noise when damaged as intended. I had problems with other sound implementation, because C# is a new language to me.

--- scripts/Key.cs
+++ scripts/Key.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class Key : Area2D
 {
-    [Export] private KeyManager keyManager;
-    [Export] private int opensDoorID;
-    
-    public override void _Ready()
-    {
-        BodyEntered += OnEntered;
-    }
+	[Export] private KeyManager keyManager;
+	[Export] private int opensDoorID;
+	
+	public override void _Ready()
+	{
+		BodyEntered += OnEntered;
+	}
 
-    private void OnEntered(Node2D node)
-    {
-        if (node is not CharacterBody2D) return;
-        keyManager.AddKey(opensDoorID);
-        Visible = false;
-        BodyEntered -= OnEntered;
-    }
+	private void OnEntered(Node2D node)
+	{
+		if (node is not CharacterBody2D) return;
+		keyManager.AddKey(opensDoorID);
+		Visible = false;
+		BodyEntered -= OnEntered;
+	}
 }
