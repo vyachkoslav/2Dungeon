ff6b7480469bbf5d9b414d8cbe75292029c31a37
Vyacheslav Denisov <vyach.denisov@gmail.com>
2024-12-05 17:24:05 +0200

    Added Esc as pause menu hotkey and pause menu uses level logic

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -1,36 +1,32 @@
-extends CanvasLayer
+extends Panel
 
-@onready var pause_panel: Panel = $PausePanel
-@onready var main_menu: Button = $"PausePanel/VBoxContainer/Main Menu"
+@export var context: PlayerContext
+@export var pause_panel: Node
 
-
-func resume():
+func resume(): 
 	get_tree().paused = false
 	pause_panel.visible = false
 	
 func paused():
 	get_tree().paused = true
 	pause_panel.visible = true
-
-
-
+	
 func _on_resume_pressed() -> void:
 	resume()
 	
-
-
 func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
+	context.level.quit_game()
+	
 func _on_pause_button_pressed() -> void:
 	if get_tree().paused:
 		resume()  # If already paused, resume the game
 	else:
 		paused()
-
-
-
+		
 func _on_main_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	resume()
+	context.level.return_to_menu()
+	
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("menu")):
+		_on_pause_button_pressed()
