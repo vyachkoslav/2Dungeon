f8377af882ddc9a84ff2c893b17394437aa74a13
Vyacheslav Denisov <vyach.denisov@gmail.com>
2024-12-05 17:23:18 +0200

    Fixed tabulation in KeyManager

--- scripts/KeyManager.cs
+++ scripts/KeyManager.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 
 public partial class KeyManager : Node
 {
-	private List<int> keys = new List<int>();
-	public void AddKey(int id)
-	{
-		if(!keys.Contains(id))
-			keys.Add(id);
-	}
-	public bool HasKeyFor(int doorID)
-	{
-		return keys.Contains(doorID);
-	}
+    private List<int> keys = new List<int>();
+    public void AddKey(int id)
+    {
+        if(!keys.Contains(id))
+            keys.Add(id);
+    }
+    public bool HasKeyFor(int doorID)
+    {
+        return keys.Contains(doorID);
+    }
 }
